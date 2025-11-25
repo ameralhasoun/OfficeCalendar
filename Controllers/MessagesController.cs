9cd3e9f97d2f29eb932dee299b4e5aa620b504a3
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-11-25 16:13:35 +0100

    update Scenario-7a

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,18 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using OfficeCalendar.Models;
 using OfficeCalendar.Services;
-using Filter.AdminRequired;
 using Filter.UserRequired;
 
 namespace OfficeCalendar.Controllers;
 
-[Route("api/v1/Message")]
-public class MessagesController : Controller
+[Route("api/v1/MessageScenario7a")]
+public class MessagesControllerScenario7a : Controller
 {
     private readonly IMessageService _messageService;
     private readonly IEventsService _eventService;
 
-    public MessagesController(IMessageService messageService, IEventsService eventService)
+    public MessagesControllerScenario7a(IMessageService messageService, IEventsService eventService)
     {
         _messageService = messageService;
         _eventService = eventService;
@@ -27,42 +26,17 @@ public class MessagesController : Controller
     {
         int? userId = await _eventService.GetUserId(GetUserSessionKey());
         if (userId == null) return BadRequest("User not found.");
-        var messages = await _messageService.GetMessagesByUserId((int)userId);
+
+        List<Message>? messages = null;
 
         int totalMessages = messages.Count;
         int readMessages = messages.Count(m => m.BeenRead);
-
-        double readRatio = totalMessages == 0 ? 0 : (double)readMessages / totalMessages;
 
         return Ok(new
         {
             total = totalMessages,
             read = readMessages,
-            ratio = readRatio
+            ratio = 0
         });
-    }
-
-    [UserRequired]
-    [HttpPut]
-    public async Task<IActionResult> UpdateMessageRead([FromQuery] int messageId)
-    {
-        int? userId = await _eventService.GetUserId(GetUserSessionKey());
-        if (userId == null) return BadRequest("User not found.");
-        bool check = await _messageService.MessageRead((int)userId, messageId);
-
-        if (check) return Ok("Message read status has been updated.");
-        return BadRequest("Message read status could not be updated.");
-    }
-
-    [UserRequired]
-    [HttpPost]
-    public async Task<IActionResult> PostMessage([FromQuery] int sendToUid, [FromBody] Message message)
-    {
-        int? currentUid = await _eventService.GetUserId(GetUserSessionKey());
-        if (currentUid == null) return BadRequest("User not found.");
-        if (currentUid == sendToUid) return BadRequest("Cannot send a message to yourself.");
-
-        bool success = await _messageService.CreateMessage(message, sendToUid, (int)currentUid);
-        return success ? Ok("Message has been sent!") : BadRequest("User does not exist.");
     }
 }
